59b2c8e2ee7f86a402443afda33d7b482e11276d
Binay Kumar Verma <binaverma01@gmail.com>
2019-01-29 06:00:33 +0530

    Implemented REST-based API, Stream create and edit

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamList from "./streams/StreamList";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamShow from "./streams/StreamShow";
 import StreamDelete from "./streams/StreamDelete";
 import Header from "./Header";
+import history from "../history";
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit" component={StreamEdit} />
-          <Route path="/streams/show" component={StreamShow} />
-          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/edit/:id" component={StreamEdit} />
+          <Route path="/streams/show/:id" component={StreamShow} />
+          <Route path="/streams/delete/:id" component={StreamDelete} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
